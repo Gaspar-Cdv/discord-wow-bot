276b2d751f3a5632450a019d443677fa60d2bcc6
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-12-22 23:30:59 +0100

    feat: create pong command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -5,6 +5,16 @@ interface Command {
 	execute: (interaction: ChatInputCommandInteraction<CacheType>) => Promise<void>
 }
 
-const commands: Command[] = []
+const commands: Command[] = [
+	{
+		data: {
+			name: 'ping',
+			description: 'Respond with pong',
+		},
+		execute: async (interaction) => {
+			await interaction.reply('Pong')
+		}
+	}
+]
 
 export default commands
