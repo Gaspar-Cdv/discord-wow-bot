7d2c7f4d73bf025634d9f75041dab66f11c99983
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2022-12-23 14:49:55 +0100

    feat: create blizzardAPIService

---
+++ src/commands/service/blizzardAPI.ts
@@ -0,0 +1,104 @@
+import axios from 'axios'
+import FormData from 'form-data'
+import config from '../../config.json'
+import { CharacterAchievementsAPIResponse } from '../../types/blizzard/character/achievements'
+import { CharacterAchievementsStatisticsAPIResponse } from '../../types/blizzard/character/achievementsStatistics'
+import { CharacterAPIResponse } from '../../types/blizzard/character/character'
+import { CharacterEquipmentAPIResponse } from '../../types/blizzard/character/equipment'
+import { CharacterQuestsAPIResponse } from '../../types/blizzard/character/quests'
+import { CharacterQuestsCompletedAPIResponse } from '../../types/blizzard/character/questsCompleted'
+import { CharacterStatisticsAPIResponse } from '../../types/blizzard/character/statistics'
+import { CharacterStatusAPIResponse } from '../../types/blizzard/character/status'
+
+type Namespace = 'profile-eu' | 'static-eu' | 'dynamic-eu'
+
+interface OAuthData {
+	access_token: string
+	token_type: string
+	expires_in: number
+	sub: string
+}
+
+class BlizzardAPIService {
+	private ACCESS_TOKEN_URL = 'https://oauth.battle.net/token'
+	private API_BASE_URL = 'https://eu.api.blizzard.com'
+
+	getCharacter = async <T = CharacterAPIResponse>(realm: string, character: string, subPath?: string): Promise<T> => {
+		let url = `profile/wow/character/${realm.toLowerCase()}/${character.toLowerCase()}`
+		if (subPath != null) {
+			url += `/${subPath}`
+		}
+		return this.fetchBlizzardApi<T>(url)
+	}
+
+	getEquipment = async (realm: string, character: string): Promise<CharacterEquipmentAPIResponse> => {
+		return this.getCharacter(realm, character, 'equipment')
+	}
+
+	getAchievements = async (realm: string, character: string): Promise<CharacterAchievementsAPIResponse> => {
+		return this.getCharacter(realm, character, 'achievements')
+	}
+
+	getAchievementsStatistics = async (realm: string, character: string): Promise<CharacterAchievementsStatisticsAPIResponse> => {
+		return this.getCharacter(realm, character, 'achievements/statistics')
+	}
+
+	getQuests = async (realm: string, character: string): Promise<CharacterQuestsAPIResponse> => {
+		return this.getCharacter(realm, character, 'quests')
+	}
+
+	getQuestsCompleted = async (realm: string, character: string): Promise<CharacterQuestsCompletedAPIResponse> => {
+		return this.getCharacter(realm, character, 'quests/completed')
+	}
+
+	getStatistics = async (realm: string, character: string): Promise<CharacterStatisticsAPIResponse> => {
+		return this.getCharacter(realm, character, 'statistics')
+	}
+
+	getStatus = async (realm: string, character: string): Promise<CharacterStatusAPIResponse> => {
+		return this.getCharacter(realm, character, 'status')
+	}
+
+	private fetchBlizzardApi = async <T = any> (path: string, namespace: Namespace = 'profile-eu'): Promise<T> => {
+		const url = new URL(path, this.API_BASE_URL)
+		url.searchParams.append('locale', 'fr_FR')
+	
+		try {
+			const accessToken = await this.getAccessToken()
+	
+			const { data } = await axios.get<T>(url.toString(), {
+				headers: {
+					'Authorization': `Bearer ${accessToken}`,
+					'Battlenet-Namespace' : namespace
+				}
+			})
+	
+			return data
+		} catch (error) {
+			console.error(error)
+			throw error
+		}
+	}
+
+	private getAccessToken = async () => {
+		const body = new FormData()
+		body.append('grant_type', 'client_credentials')
+	
+		try {
+			const { data } = await axios.post<OAuthData>(this.ACCESS_TOKEN_URL, body, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+					'Authorization': `Basic ${Buffer.from(config.blizzard.clientId + ':' + config.blizzard.secret).toString('base64')}`
+				}
+			})
+	
+			return data.access_token
+		} catch (e) {
+			console.error(e)
+			return null
+		}
+	}
+
+}
+
+export const blizzardAPIService = new BlizzardAPIService()
